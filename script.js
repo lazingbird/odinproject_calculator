79fc04f8421bb217cc872cac5b7c3ca75d9c0da7
Lucas <lazingbird@protonmail.com>
2022-10-08 19:22:08 -0300

    add keyboard support and sounds

--- script.js
+++ script.js
@@ -13,6 +13,9 @@ function multiply(a, b){
 }
 
 function divide(a, b){
+    if (b === 0){
+        return "You can't divide a number by 0"
+    }
     return a / b;
 }
 
@@ -27,29 +30,40 @@ let currentOperator = null
 
 // Query page elements
 
+window.addEventListener("keydown", keyboardHandle)
 const operation = document.querySelector("#op-text");
 const result = document.getElementById("result-text")
 const insertNumber = document.querySelectorAll("[data-number]")
 const insertOperator = document.querySelectorAll("[data-op")
 const equal = document.getElementById("equal")
-const clearDisplay = document.querySelector("#clear")
+const point = document.getElementById("point")
+const clearDisplay = document.getElementById("clear")
+const backspace = document.getElementById("delete")
 
 linebreak = document.createElement("br");
 
 // Functions to interact with HTML
 
 function insertNum(number){
-    if (firstNumber === ""){
-        const operation = document.querySelector("#op-text");
-        operation.textContent += number;
-        displayValue = operation.textContent;
-        lastPressed = "number"
+    clickSound1.currentTime = 0;
+    clickSound1.play()
+    if (operation.textContent.length >= 27){
+        return
     }
+
     else{
-        const operation = document.querySelector("#op-text");
-        operation.textContent += number;
-        secondNumber += number
-        lastPressed = "number"
+        if (firstNumber === ""){
+            const operation = document.querySelector("#op-text");
+            operation.textContent += number;
+            displayValue = operation.textContent;
+            lastPressed = "number"
+        }
+        else{
+            const operation = document.querySelector("#op-text");
+            operation.textContent += number;
+            secondNumber += number
+            lastPressed = "number"
+        }
     }
 }
 
@@ -58,7 +72,8 @@ insertNumber.forEach((button) =>
 )
 
 function insertOp(operator){
-
+    clickSound2.currentTime = 0
+    clickSound2.play()
     if (lastPressed === "operator"){
         return
     }
@@ -87,7 +102,29 @@ insertOperator.forEach((button) =>
     button.addEventListener("click", () => insertOp(button.textContent))
 )
 
+function insertPoint(){
+    clickSound2.currentTime = 0
+    clickSound2.play()
+    if (operation.textContent.length >= 27){
+        return
+    }
+    else{
+        if (operation.textContent === ""){
+            operation.textContent += "0"
+        }
+        if (operation.textContent.includes('.')){
+            return
+        }
+        operation.textContent += '.'
+
+    }
+}
+
+point.addEventListener("click", insertPoint)
+
 function clear(){
+    clickSound3.currentTime = 0
+    clickSound3.play()
     displayValue = ""
     firstNumber = ""
     secondNumber = ""
@@ -120,13 +157,56 @@ function operate(operator, a, b){
 
 
 function evaluate(currentOperator, firstNumber, secondNumber){
-    result.textContent = operate(currentOperator, firstNumber, secondNumber)
-    displayValue = result.textContent
-    console.log(firstNumber)
-    console.log(secondNumber)
+    clickSound3.currentTime = 0
+    clickSound3.play()
+    if (secondNumber === ""){
+        return
+    }
+    else{
+        result.textContent = operate(currentOperator, firstNumber, secondNumber)
+        displayValue = result.textContent
+        console.log(firstNumber)
+        console.log(secondNumber)
+    }
+
 }
+
+function delEl(){
+    clickSound3.currentTime = 0
+    clickSound3.play()
+    if (operation.textContent.length <= 1){
+        operation.textContent = operation.textContent.toString().slice(0, -1)
+        operation.appendChild(linebreak)
+    }
+    else{
+        operation.textContent = operation.textContent.toString().slice(0, -1)
+    }
+}
+
+backspace.addEventListener("click", delEl)
+
     
 
 equal.addEventListener("click", () => evaluate(currentOperator,firstNumber,secondNumber));
 
 
+function keyboardHandle(k){
+    if (k.key > 0 && k.key <= 9){
+        insertNum(k.key)
+    }
+    if (k.key === "+" || k.key === "-" || k.key === "*" || k.key === "/"){
+        insertOp(k.key)
+    }
+    if (k.key === "."){
+        insertPoint(k.key)
+    }
+    if (k.key === "=" || k.key === "Enter"){
+        evaluate(currentOperator, firstNumber, secondNumber)
+    }
+    if (k.key === "Delete"){
+        clear()
+    }
+    if (k.key === "Backspace"){
+        delEl();
+    }
+}
